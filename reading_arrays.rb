850da31d23f443c46816ea14567490062102bce8
haleykeizur <haleykz@yahoo.com>
2018-07-25 16:17:31 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -4,31 +4,31 @@ def student_names
   student_names[0]
 end
 
-puts student_names[0]
+puts "student_names[0]"
 
 def student_names
   student_names[3]
 end
 
-puts student_names[3]
+puts "student_names[3]"
 
 def student_names
   student_names[5]
 end
 
-puts student_names[5]
+puts "student_names[5]"
 
 def student_names
   student_names.first
 end
 
-puts student_names.first
+puts "student_names.first"
 
 def student_names
   student_names.last
 end
 
-puts student_names.last
+puts "student_names.last"
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
